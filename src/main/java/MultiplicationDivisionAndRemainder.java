36d0136efd778c45a95df9c5a316db31fe9dbc81
Pabeu <psztul@softserveinc.com>
2022-08-08 23:27:28 +0200

    initial commit

---
+++ src/main/java/MultiplicationDivisionAndRemainder.java
@@ -0,0 +1,39 @@
+public class MultiplicationDivisionAndRemainder {
+    private double multiplication;
+    private double division;
+    private double divRemainder;
+
+    public MultiplicationDivisionAndRemainder(double multiplication, double division, double divRemainder) {
+        this.multiplication = multiplication;
+        this.division = division;
+        this.divRemainder = divRemainder;
+    }
+
+    public double getMultiplication() {
+        return multiplication;
+    }
+
+    public double getDivision() {
+        return division;
+    }
+
+    public double getDivRemainder() {
+        return divRemainder;
+    }
+
+    public void setMultiplication(double multiplication) {
+        this.multiplication = multiplication;
+    }
+
+    public void setDivision(double division) {
+        this.division = division;
+    }
+
+    public void setDivRemainder(double divRemainder) {
+        this.divRemainder = divRemainder;
+    }
+
+    public double calculateMultiplicationDivisionAndDivRemainderResult(double x, double y){
+        return x*y;
+    }
+}
---
+++ src/test/java/CalculationTest.java
@@ -0,0 +1,8 @@
+public class CalculationTest {
+
+    public static void main(String[] args) {
+        MultiplicationDivisionAndRemainder dupa = new MultiplicationDivisionAndRemainder(5,5,5);
+        System.out.println(dupa.calculateMultiplicationDivisionAndDivRemainderResult(5,7));
+    }
+
+}
